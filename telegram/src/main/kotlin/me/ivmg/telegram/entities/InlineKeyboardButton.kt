0746862528dd193ee5f3bc478f857d72459bd6c5
Iván Martín <ivanmartinga@gmail.com>
2018-04-22 21:29:22 +0200

    Styling and typo

--- telegram/src/main/kotlin/me/ivmg/telegram/entities/InlineKeyboardButton.kt
+++ telegram/src/main/kotlin/me/ivmg/telegram/entities/InlineKeyboardButton.kt
@@ -3,8 +3,8 @@ package me.ivmg.telegram.entities
 import com.google.gson.annotations.SerializedName as Name
 
 data class InlineKeyboardButton(
-        val text: String,
-        val url: String? = null,
-        @Name("callback_data") val callbackData: String? = null,
-        @Name("switch_inline_query") val switchInlineQuery: String? = null
+    val text: String,
+    val url: String? = null,
+    @Name("callback_data") val callbackData: String? = null,
+    @Name("switch_inline_query") val switchInlineQuery: String? = null
 ) : ReplyMarkup
--- telegram/src/main/kotlin/me/ivmg/telegram/entities/InlineKeyboardMarkup.kt
+++ telegram/src/main/kotlin/me/ivmg/telegram/entities/InlineKeyboardMarkup.kt
@@ -3,8 +3,10 @@ package me.ivmg.telegram.entities
 import com.google.gson.Gson
 import com.google.gson.annotations.SerializedName
 
-class InlineKeyboardMarkup(@SerializedName("inline_keyboard")
-                           val inlineKeyboard: List<List<InlineKeyboardButton>>) : ReplyMarkup {
+data class InlineKeyboardMarkup(
+    @SerializedName("inline_keyboard")
+    val inlineKeyboard: List<List<InlineKeyboardButton>>
+) : ReplyMarkup {
     private companion object {
         val GSON = Gson()
     }
