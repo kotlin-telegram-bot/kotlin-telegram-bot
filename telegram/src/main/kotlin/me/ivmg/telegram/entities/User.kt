159b274759cb9df4ba4d746c677d3a0a22827912
Iván Martín <ivanmartinga@gmail.com>
2018-07-01 11:13:32 +0200

    Make some user properties nullable

--- telegram/src/main/kotlin/me/ivmg/telegram/entities/User.kt
+++ telegram/src/main/kotlin/me/ivmg/telegram/entities/User.kt
@@ -5,8 +5,8 @@ import com.google.gson.annotations.SerializedName as Name
 data class User(
     var id: Long,
     @Name("is_bot") val isBot: Boolean,
-    @Name("first_name") val firstName: String,
-    @Name("last_name") val lastName: String,
+    @Name("first_name") val firstName: String?,
+    @Name("last_name") val lastName: String?,
     val username: String,
-    @Name("language_code") val languageCode: String
+    @Name("language_code") val languageCode: String?
 )
