7b9c36e6693cfb5b7ae110feb26535f5abf0b10b
Iván Martín <ivanmartinga@gmail.com>
2018-07-03 00:32:19 +0200

    Make first name non nullable

--- telegram/src/main/kotlin/me/ivmg/telegram/entities/User.kt
+++ telegram/src/main/kotlin/me/ivmg/telegram/entities/User.kt
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName as Name
 data class User(
     var id: Long,
     @Name("is_bot") val isBot: Boolean,
-    @Name("first_name") val firstName: String?,
+    @Name("first_name") val firstName: String,
     @Name("last_name") val lastName: String?,
     val username: String,
     @Name("language_code") val languageCode: String?
