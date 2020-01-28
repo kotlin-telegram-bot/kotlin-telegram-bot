3ca4081e26f5cd654ac0793e7321c1ce58628ee2
Ilia Griaznov <elihun@Ilias-MacBook-Pro.local>
2020-01-28 18:25:44 +0300

    add switch_inline_query_current_chat property to InlineKeyboardButton

--- telegram/src/main/kotlin/me/ivmg/telegram/entities/InlineKeyboardButton.kt
+++ telegram/src/main/kotlin/me/ivmg/telegram/entities/InlineKeyboardButton.kt
@@ -6,5 +6,6 @@ data class InlineKeyboardButton(
     val text: String,
     val url: String? = null,
     @Name("callback_data") val callbackData: String? = null,
-    @Name("switch_inline_query") val switchInlineQuery: String? = null
+    @Name("switch_inline_query") val switchInlineQuery: String? = null,
+    @Name("switch_inline_query_current_chat") val switchInlineQueryCurrentChat: String? = null
 ) : ReplyMarkup
