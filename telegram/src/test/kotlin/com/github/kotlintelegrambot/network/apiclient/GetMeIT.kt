7765f47a61e85fe941b490465b2727e686aabf5e
Víctor J. García <vjgarciaw@gmail.com>
2020-05-30 16:59:40 +0200

    Update 'User' model
    * Add 'can_join_groups', 'can_read_all_group_messages' and 'supports_inline_queries' fields to the 'User' model.
    
    * Add tests for the 'getMe' API operation.

---
+++ telegram/src/test/kotlin/com/github/kotlintelegrambot/network/apiclient/GetMeIT.kt
@@ -0,0 +1,48 @@
+package com.github.kotlintelegrambot.network.apiclient
+
+import com.github.kotlintelegrambot.entities.User
+import junit.framework.TestCase.assertEquals
+import okhttp3.mockwebserver.MockResponse
+import org.junit.jupiter.api.Test
+
+class GetMeIT : ApiClientIT() {
+
+    @Test
+    fun `getMe response is correctly returned`() {
+        givenAnyGetMeResponse()
+
+        val getMeResult = sut.getMe().execute()
+
+        val expectedUser = User(
+            id = 482352699,
+            isBot = true,
+            firstName = "Rick",
+            username = "pickleman",
+            canJoinGroups = true,
+            canReadAllGroupMessages = false,
+            supportsInlineQueries = false
+        )
+        assertEquals(expectedUser, getMeResult.body()?.result)
+    }
+
+    private fun givenAnyGetMeResponse() {
+        val getMeResponse = """
+            {
+                "ok": true,
+                "result": {
+                    "id": 482352699,
+                    "is_bot": true,
+                    "first_name": "Rick",
+                    "username": "pickleman",
+                    "can_join_groups": true,
+                    "can_read_all_group_messages": false,
+                    "supports_inline_queries": false
+                }
+            }
+        """.trimIndent()
+        val mockedResponse = MockResponse()
+            .setResponseCode(200)
+            .setBody(getMeResponse)
+        mockWebServer.enqueue(mockedResponse)
+    }
+}
