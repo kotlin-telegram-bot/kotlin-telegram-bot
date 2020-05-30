ad7178fd8c0699beb976290cf466c3a1aa2c8477
Víctor Julián García Granado <vjgarciaw@gmail.com>
2020-05-30 17:16:26 +0200

    Update 'User' model (#76)
    * Add 'can_join_groups', 'can_read_all_group_messages' and 'supports_inline_queries' fields to the 'User' model.
    
    * Add tests for the 'getMe' API operation.

--- telegram/src/main/kotlin/com/github/kotlintelegrambot/entities/User.kt
+++ telegram/src/main/kotlin/com/github/kotlintelegrambot/entities/User.kt
@@ -1,12 +1,18 @@
 package com.github.kotlintelegrambot.entities
 
-import com.google.gson.annotations.SerializedName as Name
+import com.google.gson.annotations.SerializedName
 
+/**
+ * Represents a Telegram user or bot.
+ */
 data class User(
-    var id: Long,
-    @Name("is_bot") val isBot: Boolean,
-    @Name("first_name") val firstName: String,
-    @Name("last_name") val lastName: String? = null,
-    val username: String? = null,
-    @Name("language_code") val languageCode: String? = null
+    @SerializedName("id") val id: Long,
+    @SerializedName("is_bot") val isBot: Boolean,
+    @SerializedName("first_name") val firstName: String,
+    @SerializedName("last_name") val lastName: String? = null,
+    @SerializedName("username") val username: String? = null,
+    @SerializedName("language_code") val languageCode: String? = null,
+    @SerializedName("can_join_groups") val canJoinGroups: Boolean? = null,
+    @SerializedName("can_read_all_group_messages") val canReadAllGroupMessages: Boolean? = null,
+    @SerializedName("supports_inline_queries") val supportsInlineQueries: Boolean? = null
 )
