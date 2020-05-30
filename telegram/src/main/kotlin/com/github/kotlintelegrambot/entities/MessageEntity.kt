776a5251bcde0cb89509ea3fde9dfa8f9d9fb71b
Víctor Julián García Granado <vjgarciaw@gmail.com>
2020-05-30 17:55:28 +0200

    Update 'MessageEntity' model (#77)
    * Add 'language' field to 'MessageEntity' model.
    
    * Represent 'type' field in 'MessageEntity' model with an enum class containing all the possible types instead of using a string.
    
    * Add a test for 'EnumRetrofitConverterFactoryTest' to ensure that enums nested inside a class are correctly decoded.

--- telegram/src/main/kotlin/com/github/kotlintelegrambot/entities/MessageEntity.kt
+++ telegram/src/main/kotlin/com/github/kotlintelegrambot/entities/MessageEntity.kt
@@ -1,9 +1,48 @@
 package com.github.kotlintelegrambot.entities
 
+import com.google.gson.annotations.SerializedName
+
+/**
+ * Represents one special entity in a text message. For example, hashtags, usernames, URLs, etc.
+ */
 data class MessageEntity(
-    val type: String,
-    val offset: Int,
-    val length: Int,
-    val url: String? = null,
-    val user: User? = null
-)
+    @SerializedName("type") val type: Type,
+    @SerializedName("offset") val offset: Int,
+    @SerializedName("length") val length: Int,
+    @SerializedName("url") val url: String? = null,
+    @SerializedName("user") val user: User? = null,
+    @SerializedName("language") val language: String? = null
+) {
+    enum class Type {
+        @SerializedName("mention")
+        MENTION,
+        @SerializedName("hashtag")
+        HASHTAG,
+        @SerializedName("cashtag")
+        CASHTAG,
+        @SerializedName("bot_command")
+        BOT_COMMAND,
+        @SerializedName("url")
+        URL,
+        @SerializedName("email")
+        EMAIL,
+        @SerializedName("phone_number")
+        PHONE_NUMBER,
+        @SerializedName("bold")
+        BOLD,
+        @SerializedName("italic")
+        ITALIC,
+        @SerializedName("underline")
+        UNDERLINE,
+        @SerializedName("strikethrough")
+        STRIKETHROUGH,
+        @SerializedName("code")
+        CODE,
+        @SerializedName("pre")
+        PRE,
+        @SerializedName("text_link")
+        TEXT_LINK,
+        @SerializedName("text_mention")
+        TEXT_MENTION
+    }
+}
