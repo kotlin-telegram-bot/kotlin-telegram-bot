8f376371b4b98d1ad0ac161975d61aae40defd33
Victor <annimon119@gmail.com>
2020-08-06 11:53:10 +0300

    Add thumb field to StickerSet

--- telegram/src/main/kotlin/com/github/kotlintelegrambot/entities/stickers/StickerSet.kt
+++ telegram/src/main/kotlin/com/github/kotlintelegrambot/entities/stickers/StickerSet.kt
@@ -1,5 +1,6 @@
 package com.github.kotlintelegrambot.entities.stickers
 
+import com.github.kotlintelegrambot.entities.files.PhotoSize
 import com.google.gson.annotations.SerializedName as Name
 
 data class StickerSet(
@@ -7,5 +8,6 @@ data class StickerSet(
     val title: String,
     @Name("is_animated") val isAnimated: Boolean,
     @Name("contains_masks") val containsMasks: Boolean,
-    @Name("stickers") val stickers: List<Sticker>
+    @Name("stickers") val stickers: List<Sticker>,
+    val thumb: PhotoSize?
 )
