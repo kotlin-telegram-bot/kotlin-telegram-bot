910dad36a1a7dc0964b7f14f7959e1b96afaa23c
Víctor J. García <vjgarciaw@gmail.com>
2021-02-20 15:55:13 +0000

    answerInlineQuery to TelegramBotResult type

--- telegram/src/main/kotlin/com/github/kotlintelegrambot/Bot.kt
+++ telegram/src/main/kotlin/com/github/kotlintelegrambot/Bot.kt
@@ -1226,6 +1226,31 @@ class Bot private constructor(
         errorMessage
     ).call()
 
+    /**
+     * Use this method to send answers to an inline query. No more than 50 results per query are allowed.
+     *
+     * @param inlineQueryId Unique identifier for the answered query.
+     * @param inlineQueryResults A list of results for the inline query.
+     * @param cacheTime The maximum amount of time in seconds that the result of the inline query may be
+     * cached on the server. Defaults to 300.
+     * @param isPersonal Pass True, if results may be cached on the server side only for the user that sent
+     * the query. By default, results may be returned to any user who sends the same query.
+     * @param nextOffset Pass the offset that a client should send in the next query with the same text to
+     * receive more results. Pass an empty string if there are no more results or if you don't support
+     * pagination. Offset length can't exceed 64 bytes.
+     * @param switchPmText If passed, clients will display a button with specified text that switches the
+     * user to a private chat with the bot and sends the bot a start message with the parameter [switchPmParameter].
+     * @param switchPmParameter Deep-linking parameter for the /start message sent to the bot when user
+     * presses the switch button. 1-64 characters, only A-Z, a-z, 0-9, _ and - are allowed.
+     * Example: An inline bot that sends YouTube videos can ask the user to connect the bot to their YouTube
+     * account to adapt search results accordingly. To do this, it displays a 'Connect your YouTube account'
+     * button above the results, or even before showing any. The user presses the button, switches to a
+     * private chat with the bot and, in doing so, passes a start parameter that instructs the bot to
+     * return an oauth link. Once done, the bot can offer a switch_inline button so that the user can
+     * easily return to the chat where they wanted to use the bot's inline capabilities.
+     *
+     * @return True on success.
+     */
     fun answerInlineQuery(
         inlineQueryId: String,
         vararg inlineQueryResults: InlineQueryResult,
@@ -1234,7 +1259,7 @@ class Bot private constructor(
         nextOffset: String? = null,
         switchPmText: String? = null,
         switchPmParameter: String? = null
-    ) = answerInlineQuery(
+    ): TelegramBotResult<Boolean> = answerInlineQuery(
         inlineQueryId,
         inlineQueryResults.toList(),
         cacheTime,
@@ -1244,6 +1269,31 @@ class Bot private constructor(
         switchPmParameter
     )
 
+    /**
+     * Use this method to send answers to an inline query. No more than 50 results per query are allowed.
+     *
+     * @param inlineQueryId Unique identifier for the answered query.
+     * @param inlineQueryResults A list of results for the inline query.
+     * @param cacheTime The maximum amount of time in seconds that the result of the inline query may be
+     * cached on the server. Defaults to 300.
+     * @param isPersonal Pass True, if results may be cached on the server side only for the user that sent
+     * the query. By default, results may be returned to any user who sends the same query.
+     * @param nextOffset Pass the offset that a client should send in the next query with the same text to
+     * receive more results. Pass an empty string if there are no more results or if you don't support
+     * pagination. Offset length can't exceed 64 bytes.
+     * @param switchPmText If passed, clients will display a button with specified text that switches the
+     * user to a private chat with the bot and sends the bot a start message with the parameter [switchPmParameter].
+     * @param switchPmParameter Deep-linking parameter for the /start message sent to the bot when user
+     * presses the switch button. 1-64 characters, only A-Z, a-z, 0-9, _ and - are allowed.
+     * Example: An inline bot that sends YouTube videos can ask the user to connect the bot to their YouTube
+     * account to adapt search results accordingly. To do this, it displays a 'Connect your YouTube account'
+     * button above the results, or even before showing any. The user presses the button, switches to a
+     * private chat with the bot and, in doing so, passes a start parameter that instructs the bot to
+     * return an oauth link. Once done, the bot can offer a switch_inline button so that the user can
+     * easily return to the chat where they wanted to use the bot's inline capabilities.
+     *
+     * @return True on success.
+     */
     fun answerInlineQuery(
         inlineQueryId: String,
         inlineQueryResults: List<InlineQueryResult>,
@@ -1252,7 +1302,7 @@ class Bot private constructor(
         nextOffset: String? = null,
         switchPmText: String? = null,
         switchPmParameter: String? = null
-    ) = apiClient.answerInlineQuery(
+    ): TelegramBotResult<Boolean> = apiClient.answerInlineQuery(
         inlineQueryId,
         inlineQueryResults,
         cacheTime,
@@ -1260,7 +1310,7 @@ class Bot private constructor(
         nextOffset,
         switchPmText,
         switchPmParameter
-    ).call()
+    )
 
     /**
      * Use this method to get the current list of the bot's commands.
