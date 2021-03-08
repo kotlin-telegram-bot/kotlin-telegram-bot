94119f02845ccf31db81083edaf08a0172c4f26e
Víctor J. García <vjgarciaw@gmail.com>
2021-03-08 22:20:58 +0000

    answerCallbackQuery to TelegramBotResult

--- telegram/src/main/kotlin/com/github/kotlintelegrambot/Bot.kt
+++ telegram/src/main/kotlin/com/github/kotlintelegrambot/Bot.kt
@@ -1092,19 +1092,38 @@ class Bot private constructor(
 
     fun deleteChatStickerSet(chatId: ChatId) = apiClient.deleteChatStickerSet(chatId).call()
 
+    /**
+     * Use this method to send answers to callback queries sent from inline keyboards. The answer
+     * will be displayed to the user as a notification at the top of the chat screen or as an
+     * alert.
+     *
+     * @param callbackQueryId Unique identifier for the query to be answered.
+     * @param text Text of the notification. If not specified, nothing will be shown to the user,
+     * 0-200 characters.
+     * @param showAlert If true, an alert will be shown by the client instead of a notification at
+     * the top of the chat screen. Defaults to false.
+     * @param url URL that will be opened by the user's client. If you have created a Game and
+     * accepted the conditions via @Botfather, specify the URL that opens your game — note that
+     * this will only work if the query comes from a callback game button.
+     * @param cacheTime The maximum amount of time in seconds that the result of the callback
+     * query may be cached client-side. Telegram apps will support caching starting in version
+     * 3.14. Defaults to 0.
+     *
+     * @return True on success.
+     */
     fun answerCallbackQuery(
         callbackQueryId: String,
         text: String? = null,
         showAlert: Boolean? = null,
         url: String? = null,
         cacheTime: Int? = null
-    ) = apiClient.answerCallbackQuery(
+    ): TelegramBotResult<Boolean> = apiClient.answerCallbackQuery(
         callbackQueryId,
         text,
         showAlert,
         url,
         cacheTime
-    ).call()
+    )
 
     /**
      * Use this method to log out from the cloud Bot API server before launching the bot locally. You must log out the bot
