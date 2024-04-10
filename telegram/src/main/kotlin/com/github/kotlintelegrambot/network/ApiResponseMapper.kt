18013912c6a8c395b6491c2323a8f5eb7288b4f5
Zheg-al <54863680+Zheg-al@users.noreply.github.com>
2024-04-10 13:29:22 +0300

    Fix mapToTelegramBotResult function (#279)
    * Fix mapToTelegramBotResult function
    
    * after merge fix
    
    * code style fix

--- telegram/src/main/kotlin/com/github/kotlintelegrambot/network/ApiResponseMapper.kt
+++ telegram/src/main/kotlin/com/github/kotlintelegrambot/network/ApiResponseMapper.kt
@@ -1,37 +1,50 @@
 package com.github.kotlintelegrambot.network
 
 import com.github.kotlintelegrambot.types.TelegramBotResult
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 internal class ApiResponseMapper {
 
     fun <T : Any> mapToTelegramBotResult(apiResponse: CallResponse<Response<T>>): TelegramBotResult<T> {
-        fun invalidResponse() = TelegramBotResult.Error.InvalidResponse(
+        fun invalidResponse(): TelegramBotResult.Error = TelegramBotResult.Error.InvalidResponse(
             apiResponse.code(),
             apiResponse.message(),
             apiResponse.body(),
         )
 
-        if (!apiResponse.isSuccessful) {
+        fun Response<T>.getTelegramErrorOrInvalidResponse(): TelegramBotResult.Error {
+            return TelegramBotResult.Error.TelegramApi(
+                errorCode = this.errorCode ?: return invalidResponse(),
+                description = this.errorDescription ?: return invalidResponse(),
+            )
+        }
+
+        if (apiResponse.isSuccessful) {
+            val responseBody = apiResponse.body() ?: return invalidResponse()
+            return if (responseBody.ok) {
+                val telegramResult = responseBody.result ?: return invalidResponse()
+
+                TelegramBotResult.Success(telegramResult)
+            } else {
+                responseBody.getTelegramErrorOrInvalidResponse()
+            }
+        }
+
+        val responseBodyString = apiResponse.errorBody()?.string() ?: return invalidResponse()
+
+        val responseBody = try {
+            val type = object : TypeToken<Response<T>>() {}.type
+            Gson().fromJson<Response<T>>(responseBodyString, type)
+        } catch (e: Exception) {
             return TelegramBotResult.Error.HttpError(
                 apiResponse.code(),
-                apiResponse.errorBody()?.string(),
+                responseBodyString,
             )
         }
 
-        val responseBody = apiResponse.body() ?: return invalidResponse()
+        if (responseBody.ok) return invalidResponse()
 
-        if (responseBody.ok) {
-            val telegramResult = responseBody.result ?: return invalidResponse()
-
-            return TelegramBotResult.Success(telegramResult)
-        } else {
-            val telegramErrorCode = responseBody.errorCode ?: return invalidResponse()
-            val telegramErrorDescription = responseBody.errorDescription ?: return invalidResponse()
-
-            return TelegramBotResult.Error.TelegramApi(
-                errorCode = telegramErrorCode,
-                description = telegramErrorDescription,
-            )
-        }
+        return responseBody.getTelegramErrorOrInvalidResponse()
     }
 }
